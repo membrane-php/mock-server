9430e32683d025ffd6e102e442aa21712509d503
John Charman <john.charman@imhotek.net>
2025-09-29 11:18:28 +0100

    Add greater-than and less-than matchers

---
+++ src/Matcher/LessThan.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\MockServer\Matcher;
+
+use Membrane\MockServer\DTO;
+use Membrane\MockServer\Field;
+
+final readonly class LessThan implements \Membrane\MockServer\Matcher
+{
+    public function __construct(
+        private Field $field,
+        private int|float $limit,
+        private bool $inclusive = true,
+    ) {
+    }
+
+    public function matches(DTO $dto): bool
+    {
+        $value = $this->field->find($dto->request);
+
+        return is_numeric($value)
+            && ($this->inclusive
+                ? $value <= $this->limit
+                : $value < $this->limit);
+    }
+}

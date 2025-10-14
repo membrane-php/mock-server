8dc81954cb5604ac958903bf4fff193439ed2276
John Charman <john.charman@imhotek.net>
2025-10-14 10:29:04 +0100

    Test Api Operation handlers

---
+++ tests/Unit/Api/Handler/AddOperationTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\MockServer\Tests\Unit\Api\Handler;
+
+use Membrane\MockServer\Api\Command;
+use Membrane\MockServer\Api\Handler\AddOperation;
+use Membrane\MockServer\Api\Response;
+use Membrane\MockServer\Database;
+use Membrane\MockServer\Tests\Fixture;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[UsesClass(Response::class)]
+#[UsesClass(Command\AddOperation::class)]
+#[UsesClass(Database\Model\Operation::class)]
+#[\PHPUnit\Framework\Attributes\CoversClass(AddOperation::class)]
+final class AddOperationTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @param array<string, string|list<string>> $headers
+     */
+    #[Test]
+    #[DataProvider('provideCommands')]
+    public function itAddsOperation(
+        string $operationId,
+        int $responseCode,
+        array $headers,
+        string $body,
+    ): void {
+        $repository = new Fixture\Repository\Operation();
+
+        self::assertEquals(
+            new Response(
+                202,
+                new Database\Model\Operation(
+                    $operationId,
+                    $responseCode,
+                    $headers,
+                    $body
+                ),
+            ),
+            (new AddOperation($repository))(
+                new Command\AddOperation(
+                    $operationId,
+                    $responseCode,
+                    $headers,
+                    $body,
+                )),
+        );
+
+        self::assertNotNull($repository->fetchById($operationId));
+    }
+
+    /**
+     * @return \Generator<array{
+     *     0: string,
+     *     1: int,
+     *     2: array<string, string|list<string>>,
+     *     3: string,
+     * }>
+     */
+    public static function provideCommands(): \Generator
+    {
+        yield 'mvp' => [
+            'findPetById',
+            200,
+            [],
+            '',
+        ];
+
+        yield 'non-empty headers and body' => [
+            'findPetById',
+            200,
+            ['Cache-control' => ['age=180']],
+            '{"id":5}',
+        ];
+    }
+}
---
+++ tests/Unit/Api/Handler/DeleteOperationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\MockServer\Tests\Unit\Api\Handler;
+
+use Membrane\MockServer\Api\Command;
+use Membrane\MockServer\Api\Handler\DeleteOperation;
+use Membrane\MockServer\Api\Response;
+use Membrane\MockServer\Database;
+use Membrane\MockServer\Tests\Fixture;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[UsesClass(Response::class)]
+#[UsesClass(Command\DeleteOperation::class)]
+#[UsesClass(Database\Model\Operation::class)]
+#[\PHPUnit\Framework\Attributes\CoversClass(DeleteOperation::class)]
+final class DeleteOperationTest extends \PHPUnit\Framework\TestCase
+{
+    #[Test]
+    #[DataProvider('provideCommands')]
+    public function itDeletesOperation(
+        string $operationId,
+    ): void {
+        $operation = new Database\Model\Operation($operationId, 400, [], '');
+
+        $repository = new Fixture\Repository\Operation();
+        $repository->save($operation);
+
+        self::assertEquals(
+            new Response(204),
+            (new DeleteOperation($repository))(
+                new Command\DeleteOperation($operationId))
+        );
+
+        self::assertNull($repository->fetchById($operationId));
+
+    }
+
+    /**
+     * @return \Generator<array{
+     *     0: string,
+     * }>
+     */
+    public static function provideCommands(): \Generator
+    {
+        yield ['findPetById'];
+
+        yield ['list-pets'];
+    }
+}

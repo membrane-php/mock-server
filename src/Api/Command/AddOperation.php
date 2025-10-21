c0f9b09b8e7df53a7cc5b5a2482b8fc996e4c588
John Charman <john.charman@imhotek.net>
2025-10-21 13:07:58 +0100

    Add Membrane attributes to all Api Commands

--- src/Api/Command/AddOperation.php
+++ src/Api/Command/AddOperation.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Membrane\MockServer\Api\Command;
 
+use Membrane\Attribute\Placement;
+use Membrane\Attribute\SetFilterOrValidator;
+use Membrane\Filter\CreateObject\WithNamedArguments;
+
+#[SetFilterOrValidator(new WithNamedArguments(AddOperation::class), Placement::AFTER)]
 final readonly class AddOperation
 {
     /**
--- src/Api/Command/DeleteOperation.php
+++ src/Api/Command/DeleteOperation.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Membrane\MockServer\Api\Command;
 
+use Membrane\Attribute\Placement;
+use Membrane\Attribute\SetFilterOrValidator;
+use Membrane\Filter\CreateObject\WithNamedArguments;
+
+#[SetFilterOrValidator(new WithNamedArguments(DeleteOperation::class), Placement::AFTER)]
 final readonly class DeleteOperation
 {
     public function __construct(

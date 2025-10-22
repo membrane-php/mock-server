c148edfb34359f1240d68dfc93257e6cb6b44396
John Charman <john.charman@imhotek.net>
2025-10-22 15:45:59 +0100

    Add Membrane filter validators to AddOperation

--- src/Api/Command/AddOperation.php
+++ src/Api/Command/AddOperation.php
@@ -6,8 +6,19 @@ namespace Membrane\MockServer\Api\Command;
 
 use Membrane\Attribute\Placement;
 use Membrane\Attribute\SetFilterOrValidator;
+use Membrane\Attribute\Subtype;
 use Membrane\Filter\CreateObject\WithNamedArguments;
+use Membrane\Filter\Shape\Delete;
+use Membrane\Filter\Shape\Pluck;
+use Membrane\Filter\Shape\Rename;
 
+#[SetFilterOrValidator(new Pluck('default', 'response'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Delete('default'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Pluck('response', 'code', 'headers', 'body'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Delete('response'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('code', 'defaultResponseCode'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('headers', 'defaultResponseHeaders'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('body', 'defaultResponseBody'), Placement::BEFORE)]
 #[SetFilterOrValidator(new WithNamedArguments(AddOperation::class), Placement::AFTER)]
 final readonly class AddOperation
 {
@@ -17,7 +28,8 @@ final readonly class AddOperation
     public function __construct(
         public string $operationId,
         public int $defaultResponseCode,
-        public array $defaultResponseHeaders,
-        public string $defaultResponseBody,
+        #[Subtype('string')]
+        public array $defaultResponseHeaders = [],
+        public string $defaultResponseBody = '',
     ) {}
 }

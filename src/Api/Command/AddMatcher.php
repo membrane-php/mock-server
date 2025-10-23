3936c324762505a0b15419f31b0d95de6fd195a1
John Charman <john.charman@imhotek.net>
2025-10-23 10:32:30 +0100

    Add Membrane Filters to AddMatcher

--- src/Api/Command/AddMatcher.php
+++ src/Api/Command/AddMatcher.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Membrane\MockServer\Api\Command;
 
+use Membrane\Attribute\Ignored;
 use Membrane\Attribute\Placement;
 use Membrane\Attribute\SetFilterOrValidator;
+use Membrane\Attribute\Subtype;
 use Membrane\Filter\CreateObject\WithNamedArguments;
+use Membrane\Filter\Shape\Delete;
+use Membrane\Filter\Shape\Pluck;
+use Membrane\Filter\Shape\Rename;
 
+#[SetFilterOrValidator(new Pluck('matcher', 'type', 'args'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Delete('matcher'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('type', 'alias'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Pluck('response', 'code', 'headers', 'body'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Delete('response'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('code', 'responseCode'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('headers', 'responseHeaders'), Placement::BEFORE)]
+#[SetFilterOrValidator(new Rename('body', 'responseBody'), Placement::BEFORE)]
 #[SetFilterOrValidator(new WithNamedArguments(AddMatcher::class), Placement::AFTER)]
 final readonly class AddMatcher
 {
@@ -18,9 +31,11 @@ final readonly class AddMatcher
     public function __construct(
         public string $operationId,
         public string $alias,
+        #[Ignored]
         public array $args,
         public int $responseCode,
-        public array $responseHeaders,
-        public string $responseBody,
+        #[Subtype('string')]
+        public array $responseHeaders = [],
+        public string $responseBody = '',
     ) {}
 }

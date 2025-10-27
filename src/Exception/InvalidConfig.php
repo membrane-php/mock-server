0ca7725729631e85aec531ac36356805ad5f3a32
John Charman <john.charman@imhotek.net>
2025-10-27 14:44:51 +0000

    Add $field to InvalidConfig exception
    By doing so, Parent classes can:
    - catch child class exceptions in validation.
    - preprend to $field.
    - rethrow.
    
    Then, when rendering config validation output:
    - Fields can imploded and prepended to error messages.

--- src/Exception/InvalidConfig.php
+++ src/Exception/InvalidConfig.php
@@ -4,4 +4,26 @@ declare(strict_types=1);
 
 namespace Membrane\MockServer\Exception;
 
-final class InvalidConfig extends \RuntimeException implements \Membrane\MockServer\Exception {}
+final class InvalidConfig extends \RuntimeException implements \Membrane\MockServer\Exception
+{
+    /** @param list<string> $field */
+    public function __construct(
+        public array $field = [],
+        string $message = "",
+    ) {
+        parent::__construct($message);
+    }
+
+    /** @param list<string> $field */
+    public static function field(array $field): self
+    {
+        return new self($field, <<<MESSAGE
+                "field" is required and MUST be a non-empty list of strings.
+                This list identifies a field in requests
+                i.e.
+                    - ["path", "id"]
+                    - ["query", "tags"]
+                    - ["requestBody", "pet", "species"]
+                MESSAGE);
+    }
+}

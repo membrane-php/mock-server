19143c317188b460039912f07888ac93d3af21e1
John Charman <john.charman@imhotek.net>
2025-10-29 11:55:41 +0000

    Add Membrane Attributes where possible
    Some matchers could be built by Membrane already.
    For these I have added the attributes on now

--- src/Matcher/Matcher/GreaterThan.php
+++ src/Matcher/Matcher/GreaterThan.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Membrane\MockServer\Matcher\Matcher;
 
+use Membrane\Attribute\Placement;
+use Membrane\Attribute\SetFilterOrValidator;
+use Membrane\Filter\CreateObject\WithNamedArguments;
 use Membrane\MockServer\Mocking\DTO;
 use Membrane\MockServer\Mocking\Field;
 
+#[SetFilterOrValidator(new WithNamedArguments(GreaterThan::class), Placement::AFTER)]
 final readonly class GreaterThan implements \Membrane\MockServer\Matcher\Matcher
 {
     public function __construct(

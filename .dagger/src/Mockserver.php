23fdfaa21b31b2573aa5144d498c0663ddf70893
John Charman <john.charman@imhotek.net>
2025-11-10 11:34:20 +0000

    Dagger ignore all files that aren't useful to the module.

--- .dagger/src/Mockserver.php
+++ .dagger/src/Mockserver.php
@@ -6,7 +6,10 @@ namespace DaggerModule;
 
 use Dagger\Attribute\DaggerFunction;
 use Dagger\Attribute\DaggerObject;
+use Dagger\Attribute\DefaultPath;
 use Dagger\Attribute\Doc;
+use Dagger\Attribute\Ignore;
+use Dagger\Attribute\ReturnsListOfType;
 use Dagger\Container;
 use Dagger\Directory;
 
@@ -16,5 +19,23 @@ use function Dagger\dag;
 #[Doc('The Membrane MockServer Dagger module')]
 class Mockserver
 {
-
+    #[DaggerFunction]
+    public function __construct(
+        #[DefaultPath('.')]
+        #[Ignore(
+            '*.md',
+            '.dagger/',
+            '.dockerignore',
+            '.git/',
+            '.gitignore',
+            '.idea/',
+            '.cache/',
+            'dagger.json',
+            'generated/',
+            'LICENSE',
+            'storage/*.db',
+            'vendor/',
+        )]
+        private Directory $src,
+    ) {}
 }
